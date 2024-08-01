f14fd57fb19c983bfacd8028df7e6b07fbabe40e
Arturo Vargas Cuevas <avargas2705@outlook.com>
2024-08-01 11:08:48 -0600

    finish lecture 54

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,11 +1,16 @@
+import {useState} from 'react';
+
 import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept/CoreConcept.jsx";
 import TabButton from "./components/TabButton/TabButton.jsx";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
   function handleSelect (selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(setSelectedTopic);
   }
 
   return (
@@ -30,6 +35,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
